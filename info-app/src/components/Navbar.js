54699e792776a224fdc13ae325517e8b600699a7
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-02-25 22:58:59 +0000

    Update content in Navbar

--- info-app/src/components/Navbar.js
+++ info-app/src/components/Navbar.js
@@ -1,5 +1,12 @@
 import React from "react"
 
 export default function Navbar() {
-    return <h1>React Websiite</h1>
+    return (<nav>
+        <h1>Ravi's Bistro</h1>
+        <ul>
+        <li>About</li>
+        <li>Pricing</li>
+        <li>Contact</li>
+        </ul>
+        </nav>)
 }
